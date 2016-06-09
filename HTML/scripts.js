5f5c556ee8d41034576b808614a404553e6c88a2
zodiacbalance <zodiacweight@gmail.com>
2016-06-09 14:41:52 +0300

    связаны элементы parts и replics

--- HTML/scripts.js
+++ HTML/scripts.js
@@ -1,52 +1,189 @@
-var replix = [
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {}
-];
-window.onload=function() {
-    var introduction = document.getElementById("introduction"),
-        words = document.getElementsByClassName("words"),
-        checks = document.getElementsByClassName('check_character'),
-        wLength = words.length,
-        personageName;
-    introduction.style.dispaly = "none";
-    console.log({checks: checks, words: words});
-    // персонажи
-    var personages = [];
-    for (var i = 0, length_of_checks = checks.length; i < length_of_checks; i++) {
-        personages.push(checks[i].value);
-    }
-
-    for (i = 0; i < wLength; i++) {
-        personageName = words[i].getElementsByTagName('H4')[0].innerText;
-        console.log('personageName: %c' + personageName, 'color: orange');
-    }
-
-    // выяснить, какие из чекбоксов отмечены и обработать связанные с ними блоки
-    for (i = 0, length_of_checks = checks.length; i < length_of_checks; i++) {
-        // если чекбокс отмечен:
-        if (checks[i].checked) {
-            for (var j = 0, numb_of_replics = words.length; j < numb_of_replics; j++) {
-                /*if(checks[i].value==){
-
-                 }*/
+// Щелкнуть на вкладке > move to opposite group
+var replics = {
+        part1: [
+            {"ch1": ["Part1"]},
+            {"Author's words": ["There Woman-devil and Lilian are sitting in the bottom of the lake."]},
+            {"Author's words": ["There Woman-devil and Lilian are sitting in the bottom of the lake."]},
+            {"on_the_bottom_of_lake": ["<img class='inobject' src='../images/characters/^C613CE6AAFC415F362D2F98D25675C1A40396A148887E4AD3C^pimgpsh_fullsize_distr.jpg>'"]},
+            {"Woman-devil": ["Lilian, can you scratch my nails on the legs?"]},
+            {"Lilian": ["Of course, I can."]},
+            {"Woman-devil": ["Then move here, please, and do it for me. Today we can talk about your last visit to your onetime host if you want"]},
+            {"author's words": ["Lilian is swimming to Woman-devil with a cleaning stone."]},
+            {"Woman-devil": ["How are your acquainted people?"]},
+            {
+                "Lilian": ["I think there is nothing especially new. Christian is  " +
+                "<span class='from vocabulary'>fond</span>  with his engagement as usually. " +
+                "Unfortunately, he did not admit proposal to go here and knew something interesting about theextra-decomposers."]
+            },
+            {"Woman-devil": ["Did you tell him that the host of the estate is a murderer?"]},
+            {"Lilian": ["No I did not. I worried to say Christian about it."]},
+            {"Woman-devil": ["So you were right that you did not dare tot do it."]},
+            {"Lilian": ["But Christian <span class='from_vocabulary'> abdicated </span> from this idea at least."]},
+            {
+                "Woman-devil": ["It is his own right. If he does not prefer to do what you suggested him than just let him to do " +
+                "what he prefers to do. How were your last days in the aquarium of your first host where you flied recently?"]
+            },
+            {
+                "Lilian": ["Good, Lady Devil. I spent these days in the aquarium of my host, communicated with her friends" +
+                "They all met me with goodwill. We talked about my proposal as you know." +
+                "Besides I communicated with animals in Helen's aquarium before my communication with the people."]
+            },
+            {"Woman-devil": ["Do you like marine animals in her aquarium?"]},
+            {
+                "Lilian": ["Of course, I do. But, frankly, they do not differ <span class='from_vocabulary'>tangibly</span> from" +
+                "the animals I saw in the ocean."]
+            },
+            {"Woman-devil": ["Are you serious?"]},
+            {
+                "Lilian": ["Yes, I am serious. I communicated with the animals in ocean about my life in the swamp. I talked with" +
+                "the animals Helen's aquarium to the same topic. I told them the same what I told the animals in the" +
+                "ocean. I think both categories of the animals were interested in the same extent."]
+            },
+            {
+                "Woman-devil": ["It means there is no <span class='from_vocabulary'>tangible</span>difference between your " +
+                "conversation with the animals of one category and other one. But I have not seen yet any difference between the " +
+                "animals in your narrative. Maybe you wanted say that there is simillarity between them which is that all of them" +
+                "are equally friendly and interested in the same questions regarding to you?"]
+            },
+            {
+                "Lilian": ["I do not know how to say to be true. I communicated with all of them almost to the same theme. I told" +
+                "them almost the same about myself and my last life in the swamp. It is about what I communicated with" +
+                "them. I communicated with them about something com[letely other not so much."]
+            },
+            {
+                "Woman-devil": ["I have no doubts that if we talk exactly about the inhabitants and not about something other " +
+                "what may be related with them, for example, what your communication with them. we can notice that many of them" +
+                "are completely different: their view, body structure, nutrition and lifestyle - the difference isvery obvious."]
+            },
+            {
+                "author's words": ["For some moments there was continuing silence. It was <span class='from_vocabulary'>transgressed" +
+                "</span> by the words of Woman-devil:"]
+            },
+            {
+                "Woman-devil": ["But there are somesimilarities between all the marine animals which differ them from people." +
+                "Some people are so freaks what I can not say about animals."]
+            },
+            {"Lilian": ["What summons <span class='from_vocabulary'> disturbance </span> regarding people in you?"]},
+            {
+                "Woman-devil": ["Some people are so cruel and <span>relentless</span> being! I do not <span class='from_vocabulary'>cease</span> " +
+                "to be surprised by them! They are able to kill another human without a good reason. Somebody of them like the " +
+                "host of this house are such people who can kill just for joy. I found it a <span class='from_vocabulary'>quite" +
+                "</span> meaningless and absurd! I do not like it! I am able to kill, too, but only in such cases when a murdering" +
+                " has <span class='from_vocabulary'> implication</span>. It is absolutely not for fun! I am not able to kill just " +
+                "for fun. "]
+            },
+            {"Lilian": ["Is there a <span class='from_vocabulary'> gravity </span> for me regarding the host of these estae?"]},
+            {"Woman-devil": ["You will be in safety if you are under water always."]},
+            {"author's words": ["A medusa is swimming toewards to Woman-devil."]},
+            {"Woman-devil": ["I did not <span class='from_vocabulary'> summon </span> you here. What do you want from me?"]},
+            {"Medusa": ["I would like know about what you are telling."]},
+            {"Woman-devil": ["It is not regardless you, trust me."]},
+            {"Medusa": ["What is it about?"]},
+            {
+                "Woman-devil": ["It is about the human who lives in the house in one of the houses on the" +
+                "<span class='from_vocabulary'>bank</span> of this lake. He is so blackguard! But I think you will" +
+                "never meet him because you always are in the water and he always are out of it."]
+            },
+            {
+                "Lilian": ["But I listened that people can swim in water and breathe the in there in special hemlets." +
+                " Such people are called divers."]
+            },
+            {
+                "Woman-devil": ["There is an interesting question: what our host Quentin has to look for here? I will arrange" +
+                "him a wonderful swimming!"]
+            },
+            {
+                "Lilian": ["I think we must <span class='from_vocabulary'>retreat</span> this human! I do not like this fact" +
+                "that he can <span class='from_vocabulary'>imbue</span> here."]
+            },
+            {"Woman-devil": ["I do not like it, too. But I will take measures over this fact."]},
+            {"Medusa": ["What <span class='from_vocabulary'> sinful </span> is in him?"]},
+            {
+                "Woman-devil": ["Quentin is a phrenetic and <span class='from_vocabulary'>entirely ruthless</span>creature who" +
+                "is able tokill another human without a good reason, just for enjoying."]
+            },
+            {
+                "Lilian": ["People are a terrific kind of life! It is terrible! How is it possible to kill somebody only " +
+                "for enjoying!?"]
+            },
+            {"Medusa": ["I never have intentions to kill just for joy, too."]},
+            {
+                "Woman-devil": [" Yes, really, they are a terrific life specie. But there is explaination how can they be able " +
+                "to do what representatives of other species can not do. Human nature is a terrific thing. It is not what is" +
+                "nature of the special marine creature like you. There is such conception as human psychology. Besides there is" +
+                "such fact that human life goes on not in the same world where your life goes on, what is a very important" +
+                "circumstance. You live calm life in this paradise world compared with scary life of some people. Life of people" +
+                "is very rich with different events what there is no telling about life of such marine animals as you. The events" +
+                "of which human life consist are very different and can be characterized as something <span class='from_vocabulary'>" +
+                "antithetical</span>. There are such events and factors among them which can <span class='from_vocabulary'>tangibly" +
+                "</span> influence on a human. Human mind is formed under influence of these factors. I know Quentin's storie which" +
+                "is about how has he become an frenetic (маньяк)."]
+            },
+            {
+                "Woman-devil": ["The house where Quentin lives now does not have legitimate owners now and it is known for me why." +
+                "Quentin has <span class='from_vocabulary'>mercilessly</span> killed legitimacy host of this house. It was what is" +
+                "called a tragedy for adequate people."]
+            },
+            {"Lilian": ["You, must be, went to the human world to stop such people as Quentin?"]},
+            {"Woman-devil": ["Not exactly. My purpose is a bit more complicated."]},
+            {"Lilian": ["Whar should we do with Quentin?"]},
+            {
+                "Woman-devil": ["I will engage him. I <span class='from_vocabulary'>reflected</span> about this question and I have " +
+                "concluded that I can use him not badly. Maybe you will trust me soon, maybe you will not. Don't worry, please." +
+                "Anything will be alright."]
+            },
+            {"Medusa": ["How will you use him?"]},
+            {
+                "Woman-devil": ["I will use him not with dark purposes, on the contrary. Please, do not worry. As I said," +
+                "you will be in safety here. I will not let him to <span class='from_vocabulary'>imbue</span> here too" +
+                "deeply. Moreover there will be no necessarity for him to tamper in this lake according to my purposes." +
+                "I will talk with him out and very far from this lake"]
+            },
+            {"Medusa": ["You can live out of water and speak with people! It is surprising!"]},
+            {
+                "Woman-devil": ["Of course, I can. Why not? Lilian can, too. But, of course, it is better for her to keep in the" +
+                "lake and not to stick out."]
+            },
+            {"Medusa": ["Did Quentin told you why he has became a frenetic?"]},
+            {"Woman-devil": ["No, he did not."]},
+            {"Medusa": ["What storie was this with him?"]},
+            {"Woman-devil": ["Do you mean an accident in the result of which he has became a frenetic?"]},
+            {"Medusa": ["Yes."]},
+            {
+                "Woman-devil": ["I think it will be very difficult for your understanding. It is about human life. It was" +
+                "life in completely other world. You have no representation about this world. So i don't think you would" +
+                "understand the narrative."]
+            },
+            {"": [""]},
+            {"": [""]},
+            {"": [""]},
+            {"": [""]},
+            {"": [""]},
+            {"": [""]},
+            {"": [""]},
+            {"": [""]},
+            {"": [""]},
+            {"": [""]}],
+        part2: [
+            {"ch2": ["Part2"]},
+            {
+                "Voice on the terminal": ["Do not be daunted by the snake. It is not a <span class='from_vocabulary'>hazard" +
+                "</span> for you."]
+            },
+            {"image_with_snake": ["<img class='inobject' src='../images/characters/pimgpsh_fullsize_distr.jpg'>"]},
+            {"Snake": ["I want you were <span class='from_vocabulary'>pacified</span>."]},
+            {
+                "Quentin": ["Are you trying to <span class='from_vocabulary'>imbue</span> me that you are not black entity for" +
+                "a human despite the denomination which you are called?"]
             }
-        }
-        console.log({
-            checks: checks,
-            check: checks[i]
-        });
-    }
-    /*for (i=0;i<4;i++) {
-     checks[i]=document.getElementsByClassName("checkbox[i]");
-     } */
-    document.getElementById("choice").onclick = function () {
-
+        ],
+        part3: []
     };
+window.onload = function () {
+    // introduction
+    var introduction = document.getElementById("introduction");
+    introduction.style.dispaly = "none";
+    document.getElementById("contentlist");
     document.getElementById("button").onclick = function () {
         if (introduction.style.display == "none") { // если текст скрыт, он открывается
             this.style.color = "#084B8A"; // посветлее
@@ -59,4 +196,59 @@ window.onload=function() {
             introduction.style.display = "none";
         }
     };
+    // Клик на part.
+    var characters = {}, characterslist = document.getElementById("characterslist"),
+        text = document.getElementById("text")/*,
+        currentPart*/;
+    for(var part in replics){
+        document.getElementById(part).onclick = function () // назначается обработчик события -- ВНУТРИ ЦИКЛА!
+        {   // выполняется функция, привязанная к обработчику события -- ПОСЛЕ ВЫПОЛНЕНИЯ ЦИКЛА!!!
+            //console.log({id:this.id, part:this.id});
+            for(var key in replics[this.id]){
+                console.log({element:replics[this.id]});
+            }
+            //var numbofreplics = replics.length;
+            characterslist.innerHTML += "<h5>В этой части участвуют следующие герои:</h5><ul></ul>";
+            /* Программа, которая при клике по элементу parts заполняет правую область (id="text") репликами по
+             * определенному эпизоду, а элемент id="characterslist" чекбоксами с именами героев, присутствующих в эпизоде:
+             * 1. Извлекается объект 1-го уровня вложенности, вложенный в объект replics, Это будет объект part1 или
+             * part2 или part3 и так далее, элемент такого вида:
+             * partn [{"имя героя": ["реплика"]}, {"имя героя": ["реплика"]}, {"имя героя": ["реплика"]}, ...]
+             * Это будет тот элемент, в котором n соответствует текущему значению i, иными словами, ключ partn соответствует
+             * значению элемента массива parts.
+             * 2. Код (цикл), который будет осматривать этот объект partn от элемента к элементу и добавлять оттуда в
+             * объект characters новое свойство: имя героя: то же имя героя, но так, чтобы они не повторялись в объекте characters.
+             * 3. Каждый добавляемый в объект characters элемент записывается в html-элемент c id="characterslist":
+             * characterslist.innerHTML+='<div><li><input type="checkbox" class="checkcharacter" value=partn[k]>partn[k]</li></div>'
+             * где k - индекс элемента {"имя героя": ["реплика"]} в объекте partn.
+             * В любом случае, элемент {"имя героя": ["реплика"]}, добавляется к содержимому элемента id="text" под элементом
+             * id="characterslist":
+             * Услвие: if (этот элемент изображение, т.е. в значении есть <img) {
+             *       text.innerHTML+="значение";
+             * }
+             * else {
+             *   if (h4.innerText == "author's words") {
+             *  class="authorwords";
+             *  }
+             *   else {
+             *       class="words_of_char";
+             *   }
+             *   text.innerHTML+="<div class = 'authorwords' или 'words_of_char'>
+             *       <h4>ключ</h4>
+             *       <p>значение</p>
+             *   </div>"
+             * }
+             *
+             * */
+            /*Условие: если в объекте characters не содержится элемент с ключом, соответствующим ключу
+             * элемента replics[i][j] (наименованию героя или ключу изображения, то:
+             * 1. В этот объект добавляется свойство, в котором ключ и значение соответствуют ключу элемента
+             * replics[i][j]).
+             * 2. Значение элемента этого объекта выводится в контент элемента:
+             * <div><input type="checkbox" value=characters[j] class="check_character">
+             * Вне условия: в элемент id="text" добавляется элемент replics[i][j]*/
+            characterslist.innerHTML += '</ul><div><input id="choice" type="button" value="выделить реплики"></div>';
+        }
+    }
+
 };
