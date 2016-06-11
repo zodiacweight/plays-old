16c1a0c903e6009b314780b4fbaf09495e3c0955
zodiacbalance <zodiacweight@gmail.com>
2016-06-11 22:54:56 +0300

    В следующий раз: Добавить изображение вверх; Сделать, чтобы выделялись реплики выбранных героев.

--- HTML/scripts.js
+++ HTML/scripts.js
@@ -1,14 +1,23 @@
-// Щелкнуть на вкладке > move to opposite group
-var replics = {
+    // Щелкнуть на вкладке > move to opposite group
+    var p_aboutcharacters = {'About_characters': "<p>Christian has abdicated the idea to meet with the Woman-devil."+
+    "But there are other persons who have to meet her. They did not plan any meetings with this character, but there happened"+
+    "slap a demand to act not in the same way as they acted always. One of main such characters is a host of the estate where"+
+    "the lake is located. The Woman-devil lives in the lake. It is the same lake which was mentioned in the previous parts"+
+    "of these plays. It is surrounded by a few living houses with their courts. The hosts of these courts know nothing about"+
+    "this Woman-devil and many different marine animals living in the lake. But there is one moment which will change much."+
+    "The host of one of the courts will be impelled to be acquainted with Woman-devil and have a deal with her.</p> <p>She "+
+    "can kill a human when she thinks she has to do that.</p>"};
+    var replics = {
+        // replics[this.id] // this.id = part1, т.е. массив, обозначенный квадратными скобками после partn:
+        // arrayElementObject - объект, обозначенный фигурными скобками, сразу вложенный в replics[this.id]. Он содержит
+        // в себе один элемент вида ключ: значение(изображение, абзацс - слова героя или несколько таких абзацсев).
         part1: [
-            {"ch1": ["Part1"]},
             {"Author's words": ["There Woman-devil and Lilian are sitting in the bottom of the lake."]},
-            {"Author's words": ["There Woman-devil and Lilian are sitting in the bottom of the lake."]},
-            {"on_the_bottom_of_lake": ["<img class='inobject' src='../images/characters/^C613CE6AAFC415F362D2F98D25675C1A40396A148887E4AD3C^pimgpsh_fullsize_distr.jpg>'"]},
+            {"image": ["<img class='inobject' src='../images/with_characters/^C613CE6AAFC415F362D2F98D25675C1A40396A148887E4AD3C^pimgpsh_fullsize_distr.jpg>'"]},
             {"Woman-devil": ["Lilian, can you scratch my nails on the legs?"]},
             {"Lilian": ["Of course, I can."]},
             {"Woman-devil": ["Then move here, please, and do it for me. Today we can talk about your last visit to your onetime host if you want"]},
-            {"author's words": ["Lilian is swimming to Woman-devil with a cleaning stone."]},
+            {"Author's words": ["Lilian is swimming to Woman-devil with a cleaning stone."]},
             {"Woman-devil": ["How are your acquainted people?"]},
             {
                 "Lilian": ["I think there is nothing especially new. Christian is  " +
@@ -56,7 +65,7 @@ var replics = {
                 "are completely different: their view, body structure, nutrition and lifestyle - the difference isvery obvious."]
             },
             {
-                "author's words": ["For some moments there was continuing silence. It was <span class='from_vocabulary'>transgressed" +
+                "Author's words": ["For some moments there was continuing silence. It was <span class='from_vocabulary'>transgressed" +
                 "</span> by the words of Woman-devil:"]
             },
             {
@@ -74,7 +83,7 @@ var replics = {
             },
             {"Lilian": ["Is there a <span class='from_vocabulary'> gravity </span> for me regarding the host of these estae?"]},
             {"Woman-devil": ["You will be in safety if you are under water always."]},
-            {"author's words": ["A medusa is swimming toewards to Woman-devil."]},
+            {"Author's words": ["A medusa is swimming toewards to Woman-devil."]},
             {"Woman-devil": ["I did not <span class='from_vocabulary'> summon </span> you here. What do you want from me?"]},
             {"Medusa": ["I would like know about what you are telling."]},
             {"Woman-devil": ["It is not regardless you, trust me."]},
@@ -153,102 +162,108 @@ var replics = {
                 "Woman-devil": ["I think it will be very difficult for your understanding. It is about human life. It was" +
                 "life in completely other world. You have no representation about this world. So i don't think you would" +
                 "understand the narrative."]
-            },
-            {"": [""]},
-            {"": [""]},
-            {"": [""]},
-            {"": [""]},
-            {"": [""]},
-            {"": [""]},
-            {"": [""]},
-            {"": [""]},
-            {"": [""]},
-            {"": [""]}],
+            }
+            /* {"": [""]},
+             {"": [""]},
+             {"": [""]},
+             {"": [""]},
+             {"": [""]},
+             {"": [""]},
+             {"": [""]},
+             {"": [""]},
+             {"": [""]},
+             {"": [""]} */],
+        // replics[this.id]
         part2: [
-            {"ch2": ["Part2"]},
+
             {
                 "Voice on the terminal": ["Do not be daunted by the snake. It is not a <span class='from_vocabulary'>hazard" +
                 "</span> for you."]
             },
-            {"image_with_snake": ["<img class='inobject' src='../images/characters/pimgpsh_fullsize_distr.jpg'>"]},
-            {"Snake": ["I want you were <span class='from_vocabulary'>pacified</span>."]},
+            {"image": ["<img class='inobject' src='../images/with_characters/pimgpsh_fullsize_distr.jpg'>"]},
+            {
+                "Snake": ["I want you were <span class='from_vocabulary'>pacified</span>.", "Agreed?"
+                    /*["I want you were <span class='from_vocabulary'>pacified</span>."],*/
+                ]
+            },
             {
                 "Quentin": ["Are you trying to <span class='from_vocabulary'>imbue</span> me that you are not black entity for" +
                 "a human despite the denomination which you are called?"]
             }
         ],
-        part3: []
+        part3: [
+            {"Woman-devi": ["My conversation with Quentin was excellent!"]},
+            {"Lilian": ["Will we always be in safety?"]}
+        ]
     };
-window.onload = function () {
-    // introduction
-    var introduction = document.getElementById("introduction");
-    introduction.style.dispaly = "none";
-    document.getElementById("contentlist");
-    document.getElementById("button").onclick = function () {
-        if (introduction.style.display == "none") { // если текст скрыт, он открывается
-            this.style.color = "#084B8A"; // посветлее
-            this.value = "Закрыть";
-            introduction.style.display = "block";
-        }
-        else { // если текст открыт, он закрывается, подчеркивание исчезает
-            this.style.color = "#black";  // потемнее
-            this.value = "About characters";
-            introduction.style.display = "none";
-        }
-    };
-    // Клик на part.
-    var characters = {}, characterslist = document.getElementById("characterslist"),
-        text = document.getElementById("text")/*,
-        currentPart*/;
-    for(var part in replics){
-        document.getElementById(part).onclick = function () // назначается обработчик события -- ВНУТРИ ЦИКЛА!
-        {   // выполняется функция, привязанная к обработчику события -- ПОСЛЕ ВЫПОЛНЕНИЯ ЦИКЛА!!!
-            //console.log({id:this.id, part:this.id});
-            for(var key in replics[this.id]){
-                console.log({element:replics[this.id]});
+    var roles = {};
+    window.onload = function (){
+        // Клик на part.
+            var roleslist = document.getElementById("roleslist"),
+            subjectName,
+            text = document.getElementById("text"),
+            arrayElementObject;
+        // Клик по "About characters":
+        document.getElementById("about_characters").onclick = function(){
+            if(roleslist.innerHTML!="") {
+               roleslist.innerHTML="";
             }
-            //var numbofreplics = replics.length;
-            characterslist.innerHTML += "<h5>В этой части участвуют следующие герои:</h5><ul></ul>";
-            /* Программа, которая при клике по элементу parts заполняет правую область (id="text") репликами по
-             * определенному эпизоду, а элемент id="characterslist" чекбоксами с именами героев, присутствующих в эпизоде:
-             * 1. Извлекается объект 1-го уровня вложенности, вложенный в объект replics, Это будет объект part1 или
-             * part2 или part3 и так далее, элемент такого вида:
-             * partn [{"имя героя": ["реплика"]}, {"имя героя": ["реплика"]}, {"имя героя": ["реплика"]}, ...]
-             * Это будет тот элемент, в котором n соответствует текущему значению i, иными словами, ключ partn соответствует
-             * значению элемента массива parts.
-             * 2. Код (цикл), который будет осматривать этот объект partn от элемента к элементу и добавлять оттуда в
-             * объект characters новое свойство: имя героя: то же имя героя, но так, чтобы они не повторялись в объекте characters.
-             * 3. Каждый добавляемый в объект characters элемент записывается в html-элемент c id="characterslist":
-             * characterslist.innerHTML+='<div><li><input type="checkbox" class="checkcharacter" value=partn[k]>partn[k]</li></div>'
-             * где k - индекс элемента {"имя героя": ["реплика"]} в объекте partn.
-             * В любом случае, элемент {"имя героя": ["реплика"]}, добавляется к содержимому элемента id="text" под элементом
-             * id="characterslist":
-             * Услвие: if (этот элемент изображение, т.е. в значении есть <img) {
-             *       text.innerHTML+="значение";
-             * }
-             * else {
-             *   if (h4.innerText == "author's words") {
-             *  class="authorwords";
-             *  }
-             *   else {
-             *       class="words_of_char";
-             *   }
-             *   text.innerHTML+="<div class = 'authorwords' или 'words_of_char'>
-             *       <h4>ключ</h4>
-             *       <p>значение</p>
-             *   </div>"
-             * }
-             *
-             * */
-            /*Условие: если в объекте characters не содержится элемент с ключом, соответствующим ключу
-             * элемента replics[i][j] (наименованию героя или ключу изображения, то:
-             * 1. В этот объект добавляется свойство, в котором ключ и значение соответствуют ключу элемента
-             * replics[i][j]).
-             * 2. Значение элемента этого объекта выводится в контент элемента:
-             * <div><input type="checkbox" value=characters[j] class="check_character">
-             * Вне условия: в элемент id="text" добавляется элемент replics[i][j]*/
-            characterslist.innerHTML += '</ul><div><input id="choice" type="button" value="выделить реплики"></div>';
+           text.innerHTML="<h2>About characters</h2>"+p_aboutcharacters["About_characters"];
+           document.getElementById("for_content").style.borderLeft="3px solid #345693";
+           document.getElementById("contentlist").style.borderRight="none";
+        };
+        // Клик по каждому из part:
+        for (var part in replics) {
+                document.getElementById(part).onclick = function () // назначается обработчик события -- ВНУТРИ ЦИКЛА!
+                {   // выполняется функция, привязанная к обработчику события -- ПОСЛЕ ВЫПОЛНЕНИЯ ЦИКЛА!!!
+                    //console.log({id:this.id, part:this.id});
+                    document.getElementById("for_content").style.borderLeft="3px solid #345693";
+                    document.getElementById("contentlist").style.borderRight="none";
+                    roles = {};
+                    roleslist.innerHTML = "<strong>There are the following characters in this part:</strong><ul>";
+                    text.innerHTML = "<h2>" + this.id + "</h2>";
+                    for (var index in replics[this.id]) {
+                    // Цикл пробегает по part (replics[this.id]){
+                    // arrayElementObject = { "image": ["<img class='inobject' src='../images/roles/pimgpsh_fullsize_distr.jpg'>"] }
+                        arrayElementObject = replics[this.id][index];
+                        console.log({arrayElementObject: arrayElementObject});
+                        subjectName = Object.keys(arrayElementObject)[0]; // ключ (единственный) из объекта arrayElementObject:
+                        // image, author или наименование героя.
+                        if (subjectName != "image") {
+                            if (!(subjectName in roles)) {
+                                roles[subjectName] = true;
+                                roleslist.innerHTML += '<li><input type="checkbox" class="checkcharacter" value=' +
+                                subjectName + '>' + subjectName + '</li>';
+                            }
+                            if (subjectName == "Author's words") {
+                                text.innerHTML += "<div class='authorwords'> <h4>" + subjectName + "</h4><p>"+
+                                arrayElementObject[subjectName] + "</p></div>";
+                                    //arrayElementObject[subjectName] + "</p></div>";
+                            }
+                            else {
+                                text.innerHTML += "<div class='words_of_char'><h4>" + subjectName + "</h4><p>"+
+                                    arrayElementObject[subjectName] + "</p></div>";
+                            }
+                        }
+                        else {
+                            text.innerHTML += arrayElementObject['image'];
+                        }
+                    }
+                    roleslist.innerHTML += '</ul><div><input id="choicecharacter" type="button" value="выделить реплики"></div>';
+                };
         }
-    }
+        // Выбор героя
+        /* 1. Клик по кнопке - запускается функция. В этой функции:
+        * 2. Проверка всех реплик: если где-то есть реплика с текстурой, то снять текстуру;
+        * 3. Цикл бежит по чекбоксам. Если чекбокс отмечен, то запускается внутренний цикл.
+        * 4. Этот цикл бежит по всем элементам в элементе text, находит элементы, у которых значение h4 == value
+        * чекбокса и придает этим злементам текстуру.
+        * */
+        checks = document.getElementsByClassName('checkcharacter');
+        alert("choicecharacter:"+document.getElementById("choicecharacter"));
+       /* document.getElementById("choicecharacter").onclick = function () {
 
-};
+        }  */
+    };
+
+
