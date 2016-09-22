ebc10f7edfb18cdd880da7e32e0111fdaefb864d
zodiacbalance <zodiacweight@gmail.com>
2016-09-22 20:53:18 +0300

    1-е использование геттера

--- scripts.js
+++ scripts.js
@@ -1,19 +1,23 @@
 // var sum = 2 * 4;
 // console.log("Hello, sum = " + sum);
-function cls1(){
-    var // x = 'X',
-        y = 'Y';
-    return function(v, s){
-        console.log('I am back!');
-        if(s){
-            y = v;
-            console.log('y = '+y);
-        }else {
-            console.log('y: '+y);
-            return y;
+function handler(){
+    var data = {
+        x : 'X',
+        y : 'Y'
+    };
+
+    return {
+        get: function(name){
+            return data[name];
+        },
+        set: function(name, value){
+            data[name] = value;
         }
     }
 }
-var func = cls1();
-func ('Z', true);
-func(true);
+
+var getter = handler()["get"]; // эквивалентно function(name){
+                                                /*  return data[name];
+                                                 }, */
+// должно вернуть свойство объекта data.
+console.log(getter("x"));
