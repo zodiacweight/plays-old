17857e9be49b3a45ba3035b00899e7ebb37b5e94
zodiacbalance <zodiacweight@gmail.com>
2016-11-28 20:58:31 +0300

    Added array returned by function

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
  * Created by User on 23.11.2016.
  */
 function getData(key) {
-    var path = 'jsons/'+key+'.json';
+    var path = 'jsons/' + key + '.json';
     //console.log('%cpath', 'backgroun-color: lightskyblue', path);
     // 1. Создаём новый объект XMLHttpRequest
     var xhr = new XMLHttpRequest();
@@ -22,7 +22,6 @@ function getData(key) {
             window[key] = data[key];
             //console.log(key); // определено
             //handleJson[key].handle(data);
-            console.log(data);
             //buildHtmlInPrimary(key);
         }
     };
@@ -31,36 +30,37 @@ function getData(key) {
     };
 }
 function generatePrimeComponents(viewName) {
-    for (var i=0; i <2; i++) {
-        var titleOFPlay = config[viewName]["file_names"];
-        console.log('current',{
-            titleOFPlay:titleOFPlay,
-            'titleOFPlay[i]':titleOFPlay[i], // "Black_parody", "Xmarine"
-            'i':i
+    var titleOFPlay = config[viewName]["file_names"],
+        fileContents = [];
+    for (var i = 0, j = titleOFPlay.length; i < j; i++) {
+        console.log('current', {
+            'titleOFPlay[i]': titleOFPlay[i] // "Black_parody", "Xmarine"
         });
-        var comps = window[titleOFPlay[i]]["onTheBeginning"];
-        //console.log({viewName:viewName, titleOFPlay:titleOFPlay, comps:comps});
-        // $("h2")[i].$el.html(comps["header"]);
-        //$(".image")[i].$el.html(comps["imgOnPrimary"]);
-        //$(".preview")[i].$el.html(comps["preview"]);
+        // 1. Разрешаются ли цифры?
+        fileContents.push(window[titleOFPlay[i]]["onTheBeginning"]);
+        // 2. Втюхать эти элементы в div-обертку, которого еще нет
+        // 3. Втюхать этот div-обертку с ее содержимым в body по следующему коду:
+        // В скобках написать то, что будет означать обертку.
     }
+    // var template = _.template($(".componentsOfPrimary")[i])(comps); // ПЕРЕНЕСТИ!
+    // $("#myBody").html(); // ПЕРЕНЕСТИ!
+    return fileContents;
 }
 var config = {
     LoadHtmlPrimary: {
-        file_names:['Black_parody','Xmarine']            
+        file_names: ['Black_parody', 'Xmarine']
     }
 };
-var  LoadHtmlPrimary = Backbone.View.extend(
+var LoadHtmlPrimary = Backbone.View.extend(
     {
         initialize: function () {
             this.render();
         },
         render: function () {
             //var file_names = config.LoadHtmlPrimary.file_names;
-            var data  = generatePrimeComponents('LoadHtmlPrimary'); // 2 вложенных подобных шаблона заполняются данными
-            
+            var data = generatePrimeComponents('LoadHtmlPrimary'); // 2 вложенных подобных шаблона заполняются данными
             //var wrapperDiv = _.template($('#componentsOfPrimary').html())(data);
-            
+
             /*$("body").html(wrapperDiv);
             setTimeout(function () {  
                 wrapperDiv.fadeIn(2400); 
@@ -78,43 +78,42 @@ var  LoadHtmlPrimary = Backbone.View.extend(
 
 var Plays = Backbone.Model.extend(
     {
-        initialize: function(key){ // Xmarine, black_parody
+        initialize: function (key) { // Xmarine, black_parody
             // получить файл
             // проверить сохранённый скачанный файл в window[key]
             getData(key); // onLoad (hidden setInterval)
             // console.log(window[key]);
             this.getFileContents(key);  // setInterval
         },
-        getFileContents: function(key){
+        getFileContents: function (key) {
             // setTimeout start
-            var _this = this, cnt=0;
+            var _this = this, cnt = 0;
             // вызывается многократно
-            var sttm = setInterval(function(){
+            var sttm = setInterval(function () {
                 ++cnt;
                 // ..
-                if(window[key]) {
+                if (window[key]) {
                     _this.play_object = window[key];
                     //console.log('%cGot the file', 'color: blue', _this.play_object);
                     clearInterval(sttm);
-                }    
-                if(cnt>=50){
+                }
+                if (cnt >= 50) {
                     console.warn('Cannot get file');
                     clearInterval(sttm);
                 }
-            },100);
+            }, 100);
             /* Все запросы по сети выполняются асинхронно по умолчанию.
             В этот момент вызывается getData, но скрипт не ждет, пока  все, что написано в этой функции,
             выполнится, идет дальше. */
             // setTimeout finish
             //this.play_object = getData(key).then(function callbackOnSuccess(){
-                //console.log('_this.play_object', _this.play_object); // не определено
+            //console.log('_this.play_object', _this.play_object); // не определено
             //}, callbackOnError);
         }
     }
 );
 
-var xmarine = new Plays("Xmarine");
-var black_parody = new Plays("Black_parody");
+
 
 var AppRouter = Backbone.Router.extend({
     routes: {
@@ -124,14 +123,20 @@ var AppRouter = Backbone.Router.extend({
     },
     loadPrimary: function () {
         console.log("Функция вызвана!");
-        if(("Xmarine" in window)&&("Black_parody" in window)) {
-            alert("достигнуто");
-        }
-       // var primary = new LoadHtmlPrimary(); // Здесь тоже должен быть доступен ключ
-       
-        // ...
+        var xmarine = new Plays("Xmarine");
+        var black_parody = new Plays("Black_parody");
+        var val = setInterval(
+            function () {
+                if ("Xmarine" in window) {
+                    clearInterval(val);
+                    console.log("Попали");
+                    var primary = new LoadHtmlPrimary();
+                }
+                //console.log("Xmarine" in window);
+            }, 300);
+        //  // Здесь тоже должен быть доступен ключ
     },
-    loadSecondary: function() {
+    loadSecondary: function () {
 
     },
     enterToPlays: function () {
