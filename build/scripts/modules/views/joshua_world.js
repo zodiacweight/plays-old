2a5b002d5a11aba4bd8bed14f3717812df0ede73
srgg6701 <srgg67@gmail.com>
2017-07-15 16:29:52 +0300

    Fixing paths

--- build/scripts/modules/views/joshua_world.js
+++ build/scripts/modules/views/joshua_world.js
@@ -3,7 +3,7 @@ define(() => {
     return new (Backbone.View.extend({
         getData: function(){
             return {
-                path: '/contents/views/joshua_world.html',
+                path: 'contents/views/joshua_world.html',
                 self: this,
                 selector: '#joshua_world',
                 data: {
