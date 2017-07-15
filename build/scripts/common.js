132c1516a80f29d13f0f4fc872a5710f1d2f5f46
srgg6701 <srgg67@gmail.com>
2017-07-15 19:29:49 +0300

    Refactoring

---
+++ build/scripts/common.js
@@ -0,0 +1,24 @@
+$(function(){
+    var $asides;
+    var cnt=0, ntrv = setInterval(function(){
+        $asides = $('aside');
+        ++cnt;
+        if ($asides.length){
+            console.log('asides=>', $asides);
+            $asides.on('mouseenter', function(){
+                $(this).addClass('fade');
+                $(this).removeClass('fade-out');
+                console.log(this.className);
+            });
+            $asides.on('mouseleave', function(){
+                $(this).addClass('fade-out');
+                console.log(this.className);
+            });
+            clearInterval(ntrv);
+        }
+        if (cnt>10){
+            console.warn('Too long...');
+            clearInterval(ntrv);
+        }
+    },200);
+});
