656c5080eb3d5e0d7896ecc71bfe51fc2dee49e0
srgg6701 <srgg67@gmail.com>
2017-07-21 22:41:34 +0300

    Setting text injector

--- build/scripts/app.js
+++ build/scripts/app.js
@@ -1,10 +1,11 @@
 const modules_path = 'scripts/modules/';
 const contents_path = 'contents/';
+const jsons_path = 'data/jsons/';
 var temp_param = '?param=' + new Date().getTime();
 // const path = modules_path + 'modules/views/';
 
 require([   modules_path + 'common.js' + temp_param, 
-            modules_path + 'json_parser.js' + temp_param], 
+            modules_path + 'json_parser.js'], 
             (   jqueryResponse, 
                 jsonParser  ) => {
 
@@ -34,16 +35,24 @@ require([   modules_path + 'common.js' + temp_param,
             // get template and json
             // *** We need only 3 templates! ─ home (default), chapter and 404
             $.when( $.get(contents_path + 'templates/' + template + '.html' + temp_param), // template
-                    $.get(contents_path + 'data/jsons/' + view + '.json' + temp_param) // data
+                    $.getJSON(contents_path + jsons_path + view + '.json' + temp_param) // data
             ).then((tmpl, contents) => {
                     console.log('Done=>', {tmpl:tmpl, contents:contents});
                     // store View instance for further using
                     Views[view] =  new (Backbone.View.extend({
-                        // data: no_text ? contents[0] : { chapter: contents[0] },
                         data: contents[0],
                         tmpl: tmpl[0]
                     })); // console.log('check View', {view: view, 'Views[view]': Views[view]});
                     render(Views[view], no_text);
+                    // get text
+                    if (!no_text){
+                        $.getJSON(contents_path + jsons_path + 'texts/' + view + '.json' + temp_param, (text) => {
+                            var contents = jsonParser.parse(text, true);
+                            console.log('got text! =>', { text: text, contents: contents });
+                            $('#chapter_filters').html(contents.filters);
+                            $('#text').html(contents.html);
+                        });
+                    }
             }, (mess) => {
                 console.warn('Something went wrong...');
             });
--- build/scripts/modules/json_parser.js
+++ build/scripts/modules/json_parser.js
@@ -1,36 +1,69 @@
 define({
-    parse: (data) => {
+    parse: (data, isText) => {
         console.groupCollapsed('data here');
-        var setData =(prop, callback) => {
-            data[prop].forEach((row) => {
+        const setData =(dataArr, callback) => {
+            dataArr.forEach((row) => {
                 callback(row);
             });
         };
+        var templateData;
+        if (isText){
+            templateData={
+                html: '',
+                filters: ''
+            };
+            var contents = '',
+                personage,
+                personages = [];
+            //
+            setData(data, (row) => {
+                // array(Object, Object)
+                for(let prop in row){
+                    contents = row[prop]
+                    switch (prop) {
+                        case 'header': // string
+                            templateData.html += `<h4>${contents}</h4>`;
+                            break;
+                        case 'replics': // array(Object(line)=>paragraph)
+                            setData(contents, (line) => {
+                                personage = Object.keys(line)[0];
+                                templateData.html += `<strong>${personage}</strong>`;
+                                if (personages.indexOf(personage)===-1){
+                                    personages.push(personage);
+                                    templateData.filters += `<label><input type="checkbox" name="${personage}">${personage}</label>`;
+                                }
+                                setData(line[personage], (paragraph) => {
+                                    templateData.html += `<p>${paragraph}</p>`;
+                                });
+                            });
+                            break;
+                    }
+                }
+            });
 
-        for(var prop in data){
-            console.log('get data=>', { prop:prop, data:data[prop]});
-            switch (prop) {
-                case 'about_characters':
-                    var about_characters = '';
-                    setData(prop, (row) =>{
-                        about_characters += `<p>${row}</p>`;
-                    });
-                    break;
-                case 'filters':
-                    var chapters_filter = '';
-                    setData(prop, (name) =>{
-                        chapters_filter += `<label><input type="checkbox" name="${name}">${name}</label>`;
-                    });
-                    break;
+        } else {
+            // 
+            for(let prop in data){
+                console.log('get data=>', { prop:prop, data:data[prop]});
+                switch (prop) {
+                    case 'about_characters':
+                        var about_characters = '';
+                        setData(data[prop], (row) =>{
+                            about_characters += `<p>${row}</p>`;
+                        });
+                        break;
+                }
             }
-        }   console.groupCollapsed();
-        return {
-            aside: 'Something for aside',
-            header: data['header'],
-            preview: data['preview'],
-            about_characters: about_characters,
-            article: 'Some articles',
-            chapters_filter: chapters_filter
-        }
+            templateData = {
+                aside: 'Something for aside',
+                header: data['header'],
+                preview: data['preview'],
+                about_characters: about_characters
+            };
+        }   
+        
+        console.groupCollapsed();
+
+        return templateData;
     }
 });
