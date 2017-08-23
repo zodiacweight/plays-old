56f990fdfad777d33d0c5240af2d04bada82b7c9
sergei.klevtsov <sergei.klevtsov@accenture.com>
2017-08-23 15:28:59 +0300

    Refactoring

--- build.js
+++ build.js
@@ -6,14 +6,31 @@ const done = function(err) {
 };
 
 const script_path = './source/scripts/';
+[home, chapters_home, chapter_text] = ['default', 'chapters_home', 'chapter_text'];
 
-function populateTemplate(top_part, main){
+// const chaptersMod = require(`${script_path}chapters`);
+// console.log('build', {__dirname: __dirname, chaptersMod:chaptersMod});
+// chaptersMod.testData();
+
+function populateTemplate(main, body_class){
     return `<!DOCTYPE html>
 <html lang="en">
-<head>
-    ${top_part}
+    <head>
+    <meta charset="UTF-8">
+    <title>English: Amazing adventures of misterious creatures existed ever</title>
+    <script src="js/base_path.js"></script>
+    <link rel="stylesheet" type="text/css" href="styles/libs/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+    <script src="scripts/modules/common.js"></script>
+    <script src="scripts/libs/jquery.min.js"></script>
+    <!--
+        <script src="scripts/modules/json_parser.js"></script>
+        <script src="scripts/libs/underscore-min.js"></script>
+        <script src="scripts/libs/backbone-min.js"></script>
+        <script src="scripts/libs/require.js"></script>
+    -->
 </head>
-<body class="default">
+<body class="${body_class}">
     <main>
         ${main}
     </main>
@@ -23,26 +40,22 @@ function populateTemplate(top_part, main){
         <a href="#contacts">Contacts </a> &nbsp; | 
         <a href="#facebook">Facebook </a>
     </footer>
-    <script src="scripts/app.js" data-main="app"></script>
+    <!--<script src="scripts/app.js" data-main="app"></script>-->
 </body>
 </html>`;
 }
 
-const chaptersMod = require(`${script_path}chapters.js`);
-
-console.log('build', {__dirname: __dirname, chaptersMod:chaptersMod});
-chaptersMod.testData();
-
-const walk = (dir) => {
+const walk = (part) => {
     //
     const results = [];
+    const dir = `${path}${part}`;
     fs.readdir(dir, (err, list) => {
         if (err) return done(err);
         var i = 0;
         (function next() {
             var file = list[i++];
             if (!file) return done(null, results);
-            file = dir + '/' + file;
+            file = `${dir}/${file}`;
             fs.stat(file, (err1, stat) => {
                 if (stat && stat.isDirectory()) {
                     walk(file, (err2, res) => {
@@ -57,6 +70,19 @@ const walk = (dir) => {
                         }
                         let jsonParsed = JSON.parse(contents);
                         let tmpl = `<h1>${jsonParsed.description}</h1>`;
+                        switch (part) {
+                            case home:
+                                
+                                break;
+                            case chapters_home:
+                                
+                                break;
+                            case chapter_text:
+                                
+                                break;
+                            default:
+                                break;
+                        }
                         console.log('tmpl=>\n', tmpl);
                     });
                     next();
@@ -67,13 +93,13 @@ const walk = (dir) => {
     });
 };
 
-// walk(`${path}default`);
+walk(home);
 // walk(`${path}texts`);
 // walk(`${path}texts`);
 
-const template = populateTemplate('Some top content', '<h1>Hello, Dude</h1><p>Content comes here!</p>');
+const template = populateTemplate('<h1>Hello, Dude</h1><p>Content comes here!</p>');
 console.log(template);
-const file_to_save = `${__dirname}/source/html/output/index.html`;
+const file_to_save = `${__dirname}/static/index.html`;
 fs.writeFile(file_to_save, template, function(error) {
     if (error) {
       console.error(`write error: ${error.message}`);
