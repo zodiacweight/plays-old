5aeb6e12e1d6a3193178512a8c45980dc87538c3
srgg6701 <srgg67@gmail.com>
2017-09-02 17:31:57 +0300

    Setting a synchronous version of handler

--- build.js
+++ build.js
@@ -1,15 +1,23 @@
-const test_node = require('./test_node');
-
+const test_node = require('./source/scripts/test_node');
+const html = require('./source/scripts/html');
+const populateTemplate = html.populateTemplate;
+const getPagesContent = html.getPagesContent;
+// console.log('Check functions=>', {populateTemplate:populateTemplate, getPagesContent:getPagesContent});
 const fs = require('fs');
 const path = `${__dirname}/static/jsons`;
 const done = function(err, results) {
-    if (results) {
-        
-    }
-    if (err !== null)
+    if (err !== null){
+
         console.log(`
 done, return
-got err =>`, err);
+got err =>`, err, `
+-----------------`);
+        if (results) {
+            console.log(`results=>
+`, results, `
+-----------------`);
+        } 
+    }
 };
 
 const script_path = './source/scripts/';
@@ -19,137 +27,33 @@ const script_path = './source/scripts/';
 // console.log('build', {__dirname: __dirname, chaptersMod:chaptersMod});
 // chaptersMod.testData();
 
-function populateTemplate(main, body_class){
-    return `<!DOCTYPE html>
-<html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <title>English: Amazing adventures of misterious creatures existed ever</title>
-    <script src="js/base_path.js"></script>
-    <link rel="stylesheet" type="text/css" href="styles/libs/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="styles/style.css">
-    <script src="scripts/modules/common.js"></script>
-    <script src="scripts/libs/jquery.min.js"></script>
-    <!--
-        <script src="scripts/modules/json_parser.js"></script>
-        <script src="scripts/libs/underscore-min.js"></script>
-        <script src="scripts/libs/backbone-min.js"></script>
-        <script src="scripts/libs/require.js"></script>
-    -->
-</head>
-<body class="${body_class}">
-    <main>
-        ${main}
-    </main>
-    <footer>
-        <a href="#">Home </a> &nbsp; | 
-        <a href="#how_to_use">How to use </a> &nbsp; | 
-        <a href="#contacts">Contacts </a> &nbsp; | 
-        <a href="#facebook">Facebook </a>
-    </footer>
-    <!--<script src="scripts/app.js" data-main="app"></script>-->
-</body>
-</html>`;
-}
 
 function walk(part, callback) {
     //
-    let results = [];
-    //const dir = `${path}${part}`;
-    fs.readdir(part, (err, list) => {
-        if (err) return done(err);
-        var i = 0;
-        (function next() {
-            let file_name = list[i++];
-            
-            if (!file_name) return done(null, results);
-            
-            file = `${part}/${file_name}`;
-            //
-            if (fs.existsSync(file)) {
-                //console.log('Is file! => ', file);
+    const files = fs.readdirSync(part);
+    console.log('files=>', files);
+    
+    for(let i in files){
+        let file_name = files[i];
+        const file = `${part}/${file_name}`;
+        //
+        if (!fs.existsSync(file)) {
+            console.log(`No file there, skipped: ${file}`);            
+        } else {
+            console.log(`Exists: ${file}`);
+            const stat = fs.statSync(file);
+            if (stat && stat.isDirectory()) {
+                console.log('Is a dir: ', file);
+                walk(file);
             } else {
-                console.log(`No file there, skipped: ${file}`);
-            }
-            fs.stat(file, (err1, stat) => {
-                // console.log('got stat=>', stat);
-                if(err1) {
-                    console.log(`err1: ${err1}`);
-                }
-                if (stat && stat.isDirectory()) {
-                    //console.log('stat && stat.isDirectory()');
-                    walk(file, (err2, res) => {
-                        console.log(`err2: ${err2}, 
-file: ${file}
-isDirectory: `, stat.isDirectory(), `
-isFile: `, stat.isFile(), `
-`);
-                        results = results.concat(res);
-                        next();
-                    });
+                if (!stat.isFile()){
+                    console.log('NOT a file: ', file);
                 } else {
-                    //console.log('stat && stat.isDirectory()');
-                    results.push(file);
-                    /* fs.readFileSync(file, 'utf8', (err3, contents) => {
-                        if (err3) {
-                            return console.log(`
-readFile: ${file}
-err3: ${err3}
-contents: ${contents}`);
-                        } else {
-                            console.log(`
-directory: ${part}
-file_name: ${file_name}
---------------------------------------------------------------`);
-// contents: ${contents}
-                        }
-                        if (callback) {
-                            callback(part, contents);
-                        } 
-                    }); */
-                    let file_contents = fs.readFileSync(file, 'utf8', (file_error) => {
-                        console.log('file_error=>', file_error);
-                        return false;
-                    });
-
-                    if (file_contents) {
-                        if (callback) {
-                            callback(part, file_contents);
-                        }
-                    }
-                    console.log('file_contents=>', file_contents);                    
-                    next();
-                } 
-                // console.log('file=>', file);
-            });
-        })();
-    });
-}
-
-function getPagesContent(part_name, contents){
-    console.log('getPagesContent=>', {part_name:part_name});
-    //let jsonParsed = JSON.parse(contents);
-    //let tmpl = `<h1>${jsonParsed.description}</h1>`;
-    //let json_contents;
-    //let json;
-    switch (part_name) {
-        case home:
-        /* json = `${path}${home}`;
-        json_contents = fs.readFile(`${json}.json`, 'utf8', (err4, json) => {
-            console.log('json=>', JSON.parse(json));
-                let json_content = walk(home, );
-            }); */
-            break;
-        case chapters_home:
-            
-            break;
-        case chapter_text:
-            
-            break;
-        default:
-            break;
+                    console.log('IS A FILE: ', file);
+                }
+            }
+        }
     }
-    //console.log('contents=>\n', contents);
 }
 
 /* if ('file' in test_node['args']) {
---
+++ source/scripts/html.js
@@ -0,0 +1,60 @@
+//
+module.exports = {
+    populateTemplate: (main, body_class) => {
+    return `<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <title>English: Amazing adventures of misterious creatures existed ever</title>
+    <script src="js/base_path.js"></script>
+    <link rel="stylesheet" type="text/css" href="styles/libs/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+    <script src="scripts/modules/common.js"></script>
+    <script src="scripts/libs/jquery.min.js"></script>
+    <!--
+        <script src="scripts/modules/json_parser.js"></script>
+        <script src="scripts/libs/underscore-min.js"></script>
+        <script src="scripts/libs/backbone-min.js"></script>
+        <script src="scripts/libs/require.js"></script>
+    -->
+</head>
+<body class="${body_class}">
+    <main>
+        ${main}
+    </main>
+    <footer>
+        <a href="#">Home </a> &nbsp; | 
+        <a href="#how_to_use">How to use </a> &nbsp; | 
+        <a href="#contacts">Contacts </a> &nbsp; | 
+        <a href="#facebook">Facebook </a>
+    </footer>
+    <!--<script src="scripts/app.js" data-main="app"></script>-->
+</body>
+</html>`;
+    },   
+    getPagesContent: (part_name, contents) => {
+        console.log('getPagesContent=>', {part_name:part_name});
+        //let jsonParsed = JSON.parse(contents);
+        //let tmpl = `<h1>${jsonParsed.description}</h1>`;
+        //let json_contents;
+        //let json;
+        switch (part_name) {
+            case home:
+            /* json = `${path}${home}`;
+            json_contents = fs.readFile(`${json}.json`, 'utf8', (err4, json) => {
+                console.log('json=>', JSON.parse(json));
+                    let json_content = walk(home, );
+                }); */
+                break;
+            case chapters_home:
+                
+                break;
+            case chapter_text:
+                
+                break;
+            default:
+                break;
+        }
+        //console.log('contents=>\n', contents);
+    }
+}
